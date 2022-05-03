f521aaf289a7735d9a4ebbb98839b7f2a83e5e85
PiotrekPKP <peterpkppeter@gmail.com>
2022-05-03 14:46:30 +0200

    fix: Updated scrollbar

--- app.vue
+++ app.vue
@@ -14,19 +14,22 @@
 onMounted(() => {
   //i18n.locale.value = localStorage.getItem("lang") || DEFAULT_LOCALE;
   document.body.classList.add("theme");
-  document.body.classList.add(localStorage.getItem("last-theme") ?? "theme--dark");
+  document.body.classList.add(
+    localStorage.getItem("last-theme") ?? "theme--dark"
+  );
 });
 </script>
 
 <style lang="scss">
-@import "./assets/styles/themes.scss";
+@import "./assets/styles/themes";
+@import "./assets/styles/variables";
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap");
 
 * {
   font-family: "Montserrat", sans-serif;
   margin: 0;
   padding: 0;
-  transition: background-color .1s ease-in-out;
+  transition: background-color 0.1s ease-in-out;
 }
 
 body {
@@ -43,5 +46,29 @@ body {
   min-height: 100vh;
 }
 
+::-webkit-scrollbar {
+  width: 0.5em;
+  height: 0.5em;
+}
 
+::-webkit-scrollbar-button {
+  width: 0;
+  height: 0;
+}
+
+::-webkit-scrollbar-thumb {
+  background: $main-gradient;
+  border: none;
+  border-radius: 0;
+}
+
+::-webkit-scrollbar-track {
+  background: $bg--dark;
+  border: none;
+  border-radius: 0;
+}
+
+::-webkit-scrollbar-corner {
+  background: transparent;
+}
 </style>
